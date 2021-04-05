ed9b59e8d4851595d8f3c5ee977b9eebcaf0c48b
Zertal Raouf <Z.raouf97@gmail.com>
2021-04-06 01:16:53 +0200

    Update Admin, Driver Models

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Driver extends Model
+class Driver extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 }
