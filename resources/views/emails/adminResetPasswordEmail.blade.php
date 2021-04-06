aa62fb81f8593dba23cbbd81fef776f7c97e3669
Zertal Raouf <Z.raouf97@gmail.com>
2021-04-06 02:19:31 +0200

    Update adminResetPasswordEmail.blade.php

--- resources/views/emails/adminResetPasswordEmail.blade.php
+++ resources/views/emails/adminResetPasswordEmail.blade.php
@@ -3,13 +3,13 @@
     # Welcome {{$data['admin']->name}}
     # This is an email for reset your account
 
-    @component('mail::button', ['url' => url('admin/reset/password/'.$data['token'])])
+    @component('mail::button', ['url' => url('admin/reset-password/'.$data['token'])])
         Click here to reset your password
     @endcomponent
 
     or <br>
     copy this link
-    <a href="{{url('admin/reset/password/'.$data['token'])}}">{{url('admin/reset/password/'.$data['token'])}}</a><br>
+    <a href="{{url('admin/reset-password/'.$data['token'])}}">{{url('admin/reset-password/'.$data['token'])}}</a><br>
 
     Thanks,<br>
     {{ config('app.name') }}
