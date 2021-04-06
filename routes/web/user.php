8b0e71969327c8e8a6847349ca59a40d3472350a
Zertal Raouf <Z.raouf97@gmail.com>
2021-04-06 15:06:59 +0200

    Create routes/web/user.php

---
+++ routes/web/user.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('login',[\App\Http\Controllers\Web\User\UserAuthController::class,'index'])->name('login.form');
+
+Route::post('login',[\App\Http\Controllers\Web\User\UserAuthController::class,'login'])->name('login.post');
+
+Route::get('forgot-password',[\App\Http\Controllers\Web\User\UserAuthController::class,'email_form'])->name('password.form');
+
+Route::post('forgot-password',[\App\Http\Controllers\Web\User\UserAuthController::class,'forgot_password'])->name('password.post');
+
+Route::get('reset-password/{token}',[\App\Http\Controllers\Web\User\UserAuthController::class,'reset_password_form'])->name('reset.form');
+
+Route::post('reset-password/{token}',[\App\Http\Controllers\Web\User\UserAuthController::class,'reset_password'])->name('reset.post');
+
+Route::middleware('auth:admin')->group(static function(){
+    Route::any('logout',[App\Http\Controllers\Web\User\UserAuthController::class,'logout'])->name('logout');
+    Route::get('dashboard',function (){
+        return view('user.dashboard');
+    })->name('dashboard');
+});
