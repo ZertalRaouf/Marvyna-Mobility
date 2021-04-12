5d020d81c18697d720632c99c5cae6c0a3541231
Zertal Raouf <Z.raouf97@gmail.com>
2021-04-12 16:17:03 +0200

    Update 2021_04_05_225510_create_drivers_table.php

--- database/migrations/2021_04_05_225510_create_drivers_table.php
+++ database/migrations/2021_04_05_225510_create_drivers_table.php
@@ -15,6 +15,11 @@ class CreateDriversTable extends Migration
     {
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
