2c744235f4ecfabb58752c4b2cfe37dbb50861fe
Zertal Raouf <Z.raouf97@gmail.com>
2021-04-12 16:11:03 +0200

    Update driverResetPasswordEmail.blade.php

--- resources/views/emails/driverResetPasswordEmail.blade.php
+++ resources/views/emails/driverResetPasswordEmail.blade.php
@@ -1,12 +1,17 @@
 @component('mail::message')
-# Introduction
 
-The body of your message.
+    # Welcome {{$data['driver']->name}}
+    # This is an email for reset your account
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
+    @component('mail::button', ['url' => url('driver/reset-password/'.$data['token'])])
+        Click here to reset your password
+    @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
+    or <br>
+    copy this link
+    <a href="{{url('driver/reset-password/'.$data['token'])}}">{{url('driver/reset-password/'.$data['token'])}}</a><br>
+
+    Thanks,<br>
+    {{ config('app.name') }}
+
 @endcomponent
