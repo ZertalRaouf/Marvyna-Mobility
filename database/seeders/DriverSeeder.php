a0ef2062835eb8a7e7c3e0fdd67a1e0c7f1bbba6
Zertal Raouf <Z.raouf97@gmail.com>
2021-04-12 16:22:57 +0200

    Create DriverSeeder.php

---
+++ database/seeders/DriverSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Driver;
+use Illuminate\Database\Seeder;
+
+class DriverSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Driver::create([
+            'name'=>'john doe',
+            'email'=>'driver@app.com',
+            'name'=>bcrypt('password'),
+        ]);
+    }
+}
