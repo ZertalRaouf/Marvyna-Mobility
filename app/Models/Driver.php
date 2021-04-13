5bb762f7d700304b98b2519885b6e8981fe369b5
Zertal Raouf <Z.raouf97@gmail.com>
2021-04-14 00:41:06 +0200

    Update Driver.php

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -39,4 +39,8 @@ class Driver extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function circuits(){
+        return $this->hasMany(Circuit::class);
+    }
 }
