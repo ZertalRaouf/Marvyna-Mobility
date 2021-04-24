022efa50d111d9f23c9469f862e3c0259b2397d9
Zertal Raouf <Z.raouf97@gmail.com>
2021-04-24 17:36:28 +0200

    Create EstablishmentController.php

---
+++ app/Http/Controllers/Web/Admin/EstablishmentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Web\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Establishment;
+use Illuminate\Http\Request;
+
+class EstablishmentController extends Controller
+{
+    public function index(){
+        $establishments = Establishment::paginate(10);
+        return view('admin.establishments.index',compact('establishments'));
+    }
+
+    public function create(){
+        return view('admin.establishments.create');
+    }
+
+    public function store(Request $request){
+        $data = $request->validate([
+            'name'=>'required|string|max:45',
+            'address'=>'required|string',
+            'observation'=>'string|nullable'
+        ]);
+        Establishment::create($data);
+        session()->flash('success','Établissement créé avec succes');
+        return redirect()->route('admin.establishments.index');
+    }
+
+    public function show($id){
+        $establishment = Establishment::find($id);
+        if(!$establishment){
+            return abort(404,'Établissement introuvable');
+        }
+        return view('admin.establishments.show',compact('establishment'));
+    }
+
+    public function edit($id){
+        $establishment = Establishment::findOrFail($id); // We don't need the if condition
+        return view('admin.establishments.edit',compact('establishment'));
+    }
+
+    public function update($id,Request $request){
+        $data = $request->validate([
+            'name'=>'required|string|max:45',
+            'address'=>'required|string',
+            'observation'=>'string|nullable'
+        ]);
+        $establishment = Establishment::findOrFail($id);
+        $establishment->update($data);
+        session()->flash('success','Établissement modifié avec succes');
+        return redirect()->route('admin.establishments.index');
+    }
+
+    public function destroy($id){
+        $establishment = Establishment::findOrFail($id);
+        $establishment->delete();
+        session()->flash('success','Établissement supprimé avec succes');
+        return redirect()->route('admin.establishments.index');
+    }
+}
