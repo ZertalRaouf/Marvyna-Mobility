88d9151d014741914e8351da5c2c5d5cd4a64f7a
Zertal Raouf <Z.raouf97@gmail.com>
2021-05-11 06:10:36 +0200

    Update footer.blade.php

--- resources/views/admin/layouts/partials/footer.blade.php
+++ resources/views/admin/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 
 <footer class="main-footer py-2">
     <div class="float-right d-none d-sm-block">
-        <strong>Copyright &copy; 2021 <a href="javascript:void(0)" class="text-info">Groupe 10</a>.</strong> All rights
+        <strong>Copyright &copy; 2021 <a href="https://www.linkedin.com/in/zertalraouf/" class="text-info">ZERTAL Raouf</a>.</strong> All rights
         reserved.
     </div>
     <b>Version</b> 1.0
